05f08e36c8055af80c720a3083301fc2a5b482d6
Mackenzie Britton <mbbritton@gmail.com>
2013-04-17 19:05:18 -0500

    Implementation to pass tests, needs refactoring -mbb

--- src/britton/business/DeterminePriceRange.java
+++ src/britton/business/DeterminePriceRange.java
@@ -1,5 +1,6 @@
 package britton.business;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import britton.beans.Company;
@@ -7,9 +8,39 @@ import britton.beans.Customer;
 
 public class DeterminePriceRange {
 
-	public List<Company> findCompaniesWithinPriceRange(Customer customer) {
-		// TODO Auto-generated method stub
-		return null;
+	public List<Company> findCompaniesWithinPriceRange(Customer cu) {
+		List<Company> placesToShop = new ArrayList<>();
+		int coLow, coHigh;
+		
+		List<Company> companyList = new ArrayList<>();
+		companyList.add(new Company("A", 1000, 3000));
+		companyList.add(new Company("B", 6000, 10000));
+		companyList.add(new Company("C", 500, 2500));
+		
+		int cuHigh = cu.getHighRange();
+		int cuLow = cu.getLowRange();
+		
+		for(Company co : companyList){
+			coLow = co.getLowPrice();
+			coHigh = co.getHighPrice();
+			
+			if(doesCustomerFallInCompanyRange(cuLow, cuHigh, coLow, coHigh) || doesCompanyFallInCustomerRange(cuLow, cuHigh, coLow, coHigh)){
+				  placesToShop.add(co);
+			}	
+		}
+		return placesToShop;
 	}
+		//This method serves to return whether a customer falls into a company's price range
+		private boolean doesCustomerFallInCompanyRange(int cuLow, int cuHigh, int coLow, int coHigh) {
+			return cuLow >= coLow && cuLow <= coHigh || cuHigh >= coLow && cuHigh <= coHigh;
+		}
+		
+		//This method serves to return whether a company falls within a customer's price range
+		private boolean doesCompanyFallInCustomerRange(int cuLow, int cuHigh, int coLow, int coHigh){
+			return coLow >= cuLow && coLow <= cuHigh || coHigh >= cuHigh && coHigh <= cuHigh;
+		}
+		
+
+
 
 }
